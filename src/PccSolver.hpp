1588cc7bd71e6d1874b5d2de10cd5fdaef277d8c
Yasmim Cosme <yasmimcosm@gmail.com>
2026-07-06 15:30:36 -0300

    Adiciona algoritmos para 2.2 e 2.3

--- src/PccSolver.hpp
+++ src/PccSolver.hpp
@@ -2,11 +2,30 @@
 #define PCCSOLVER_HPP
 
 #include <vector>
+#include <utility>
+
+#include "Grafo.hpp"
 
 namespace PccSolver {
+
     const long long INF = 1e18;
 
-    std::vector<long long> dijkstra(int origem, const std::vector<std::vector<std::pair<int, int>>>& grafo);
+    std::vector<long long> dijkstra(
+        int origem,
+        const std::vector<std::vector<std::pair<int, int>>>& grafo
+    );
+
+    std::vector<std::vector<long long>>
+    calcularDistanciasMinimas(
+        const Grafo& g,
+        const std::vector<int>& impares
+    );
+
+    std::vector<std::pair<int,int>>
+    emparelhamentoMinimo(
+        const std::vector<int>& impares,
+        const std::vector<std::vector<long long>>& dist
+    );
 
 }
 
